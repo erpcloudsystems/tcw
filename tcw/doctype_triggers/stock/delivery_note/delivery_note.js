2f37fcd51638c0b5931720841d5c18b026ede0cc
mghoneem10 <mghoneem10@gmail.com>
2022-07-26 19:45:13 +0000

    edit js file

--- tcw/doctype_triggers/stock/delivery_note/delivery_note.js
+++ tcw/doctype_triggers/stock/delivery_note/delivery_note.js
@@ -1,4 +1,13 @@
-/*
-frappe.ui.form.on("Sales Invoice", "validate", function(frm) {
-});
-*/
+frappe.ui.form.on("Delivery Note", "refresh", function(frm){
+    if (cur_frm.doc.shipment_pdf) {
+      frm.add_custom_button("Print R2S Shipment", function(){
+          var myWin = window.open(cur_frm.doc.shipment_pdf);	});
+    }
+  });
+  
+  frappe.ui.form.on("Delivery Note", "refresh", function(frm){
+    if (cur_frm.doc.sales_invoice) {
+      frm.add_custom_button("Print Invoice", function(){
+          var myWin = window.open('/api/method/frappe.utils.print_format.download_pdf?doctype=Sales%20Invoice&_lang=ar&name='+cur_frm.doc.sales_invoice);	});
+    }
+  });
